dd84e740d120819f6362e99c0e03dcc6e91580e0
Vladimir Kofman <vladimir.kofman@intel.com>
2017-04-21 21:05:42 +0300

    TableTie renaming fixes

--- TableTie/Adapter.swift
+++ TableTie/Adapter.swift
@@ -1,6 +1,6 @@
 //
-//  TableDapter.swift
-//  TableDapter
+//  Adapter.swift
+//  TableTie
 //
 //  Created by Vladimir Kofman on 21/04/2017.
 //  Copyright © 2017 Vladimir Kofman. All rights reserved.
--- TableTie/Row.swift
+++ TableTie/Row.swift
@@ -1,6 +1,6 @@
 //
 //  Row.swift
-//  TableDapter
+//  TableTie
 //
 //  Created by Vladimir Kofman on 21/04/2017.
 //  Copyright © 2017 Vladimir Kofman. All rights reserved.
