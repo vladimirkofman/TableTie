5756a842d0e9d51017d04f81e8057890c8b96d2d
Vladimir Kofman <vladimir.kofman@intel.com>
2017-04-21 16:46:17 +0300

    Adding Example target

---
+++ Example/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Vladimir Kofman on 21/04/2017.
+//  Copyright © 2017 Vladimir Kofman. All rights reserved.
+//
+
+import UIKit
+import TableTie
+
+//struct Artist {}
+//
+//extension Artist: Row {
+//    var reuseIdentifier: String { return "storyboardCell" }
+//    
+//    var rowHeight: CGFloat { return 80 }
+//    
+//    func configure(cell: UITableViewCell) {
+//        cell.textLabel?.text = "STORYBOARD CELL"
+//    }
+//    
+//    func didSelect() {
+//        print("Selected Artist")
+//    }
+//}
+
+
+class ViewController: UIViewController {
+
+//    let adapter = Adapter([Section("Haha", [Artist()])])
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
