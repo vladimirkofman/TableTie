fda4f1ce6bdbaff01abf02b017db6f44a4a838ab
Vladimir Kofman <vladimir.kofman@intel.com>
2017-04-24 00:10:30 +0300

    Adding documentation

--- TableTie/AnyRow.swift
+++ TableTie/AnyRow.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
+/**
+ Acts as an abstract protocol for all row items to be displayed
+ All row items should conform to this protocol through Row
+ */
 public protocol AnyRow {
+    
+    /// reuseIdentifier to use when dequeing reusable cell
     var reuseIdentifier: String { get }
+    
+    /// Row Height
     var rowHeight: CGFloat { get }
+    
+    /// Called when the row is selected
     func didSelectRow(of tableView: UITableView, at indexPath: IndexPath)
     
+    /// "Private" method, don't override. Implemented in Row
     func _dequeueCell(tableView: UITableView) -> UITableViewCell
+    
+    /// "Private" method, don't override. Implemented in Row
     func _configure(cell: UITableViewCell)
 }
 
+/// Default implementations of AnyRow protocol
 public extension AnyRow {
+    
+    /// Using class name as reuseIdentifier
     var reuseIdentifier: String { return String(describing: type(of: self)) }
+    
+    /// Default row height
     var rowHeight: CGFloat { return 44 }
     
+    /// Do nothing by default
     func didSelectRow(of tableView: UITableView, at indexPath: IndexPath) {}
 }
